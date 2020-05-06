36cc61bf268464a9b8e9909612a6e17b0b6a745a
atrp87 <drewpeattie@hotmail.com>
2020-05-06 20:36:43 +0100

    null

---
+++ src/main/java/attractions/RollerCoaster.java
@@ -0,0 +1,24 @@
+package attractions;
+
+import behaviours.ISecurity;
+import behaviours.ITicketed;
+import people.Visitor;
+
+public class RollerCoaster extends Attraction implements ISecurity, ITicketed {
+
+    public RollerCoaster(String name, int rating) {
+        super(name, rating);
+    }
+
+    public boolean isAllowedTo(Visitor visitor) {
+        return false;
+    }
+
+    public double defaultPrice() {
+        return 8.40;
+    }
+
+    public double priceFor(Visitor visitor) {
+        return 0;
+    }
+}
